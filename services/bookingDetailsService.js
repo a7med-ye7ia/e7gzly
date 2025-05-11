5613b75a752215754d67a486a8ec2096151b7163
M0-Anwar <mhmdahmdmohamedahmed528@gmail.com>
2025-05-11 18:54:51 +0300

    add function to the booking details

--- services/bookingDetailsService.js
+++ services/bookingDetailsService.js
@@ -13,4 +13,22 @@ const addDetailDoc = async (doc) => {
   return await addDocument(collectionName, doc)
 }
 
-export { addDetailDoc }
+const getDetailDocById = async (documentId) => {
+  const data = await getDocumentById(collectionName, documentId)
+  if (!data) {
+    return {success: false, error: 'Document not found'}
+  }
+
+  const passengers = [numberOfSeats];
+  for (let i = 0; i < data.numberOfSeats; i++) {
+    passengers.push({
+      name: data.names[i],
+      citizenship: data.citizenship[i],
+      passportNumber: data.passportNumbers[i],
+      expirationDates: data.expirationDates[i],
+      selectedSeats: data.selectedSeats[i],
+    });
+  }
+}
+
+export { addDetailDoc, getDetailDocById}
